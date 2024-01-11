ac4459e2570f2c220902a5141afe3a948de49dc8
hyunsang4072 <hyunsang4072@gmail.com>
2024-01-10 16:50:58 -0800

    complete state

--- src/App.js
+++ src/App.js
@@ -6,29 +6,96 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+      <Counter />
+    </div>
+  );
+}
+
+
+function Counter() {
+  const [steps, setSteps] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function minusStep() {
+    setSteps((curr) => curr - 1);
+  }
+
+  function plusStep() {
+    setSteps((curr) => curr + 1);
+  }
+
+  function stepCountInc() {
+    setCount((count) => count + steps);
+  }
+
+  function stepCountDec() {
+    setCount((count) => count - steps);
+  }
+
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: '#7950f2', color: '#fff' }}
+        onClick={minusStep}>
+        -
+      </button>
+      Step: {steps}
+      <button
+        style={{ backgroundColor: '#7950f2', color: '#fff' }}
+        onClick={plusStep}>
+        +
+      </button>
+
+      <button
+        style={{ backgroundColor: '#7950f2', color: '#fff' }}
+        onClick={stepCountDec}>
+        -
+      </button>
+      Count: {count}
+      <button
+        style={{ backgroundColor: '#7950f2', color: '#fff' }}
+        onClick={stepCountInc}>
+        +
+      </button>
+      <p>
+        `{count} days from today is {date.toDateString()}.`
+      </p>
+    </div>
+  );
+}
+
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
   // eventHandler
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      setStep((curr) => curr - 1);
     };
   }
 
   // eventHandler
   function handleNext() {
     if (step < 3) {
-      setStep(step + 1);
+      setStep((curr) => curr + 1);
+      // setStep((curr) => curr + 1);
     };
   }
 
 
   return (
-    <>
-      <button className='close' onClick={() => setIsOpen(!isOpen)}>&times;</button>
+    <div>
+      <button className='close' onClick={() => setIsOpen((is) => !is)}>&times;</button>
 
       {isOpen && (<div className='steps'>
         <div className='numbers'>
@@ -54,7 +121,7 @@ export default function App() {
           </button>
         </div>
       </div>)}
-    </>
+    </div>
   );
 }
 
